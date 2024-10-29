0e793576a4ba0ab9533083fe15d4a9019027900c
Zerre <zerreytug.gll@gmail.com>
2024-10-29 18:33:54 +0100

    Afegint comentaris per clarificar les funcions a implementar

--- src/Districte.h
+++ src/Districte.h
@@ -8,8 +8,8 @@
 #include <string>
 #include <list>
 #include <set>
-#include <iostream>
 #include <map>
+#include <iostream>
 
 #include "Estudi.h"
 #include "Nacionalitat.h"
@@ -20,14 +20,30 @@ using namespace std;
 class Districte
 {
 public:
+    // @Pre: --
+    // @Post: Constructor per defecte que inicialitza tot sense cap informació extra
     Districte();
+    // @Pre: --
+    // @Post: Afegeix un habitant a les estructures de dades amb les característiques indicades pels paràmetres
     void afegir(int seccio, int codiNivellEstudis, const string &nivellEstudis, int anyNaixement, int codiNacionalitat, const string &nomNacionalitat);
+    // @Pre: --
+    // @Post: Retorna el nombre d’habitants del Districte
     long obtenirNumHabitants() const;
+    // @Pre: --
+    // @Post: Retorna l’edat mitjana dels habitants del Districte
     double obtenirEdatMitjana() const;
+    // @Pre: --
+    // @Post: Retorna el nom dels tipus d’Estudi que tenen els habitants del Districte
     set<string> resumEstudis() const;
+    // @Pre: --
+    // @Post: Retorna el nom dels països d’origen dels habitants del Districte
     set<string> resumNacionalitats() const;
-    map<int, long> obtenirHabitantsPerSeccio() const;
+    // @Pre: --
+    // @Post: Retorna el nombre d’habitants del Districte nascuts l’any anyNaixement al país amb codi codiNacionalitat
     long comptaEdatNacionalitat(int anyNaixement, int codiNacionalitat) const;
+    // @Pre: --
+    // @Post: Retorna el nombre d'habitants per Seccio
+    map<int, long> obtenirHabitantsPerSeccio() const;
 
 private:
     list<Persona> _Persones; // Llista de persones
@@ -35,7 +51,7 @@ private:
     set<Nacionalitat> _Nacionalitats; // Set de nacionalitats
     map<int, long> _habitantsSeccio; // Map amb clau com seccio i un long que representa el num. d'habitants
 
-    static const int ANY_ACTUAL = 2024;
+    static const int ANY_ACTUAL = 2024; // Es guarda en una constant l'any actual
 };
 
 #endif // DISTRICTE_H
